87b8cf4a756030165a911e4f0e6e2ace42d3ebb8
Sutrasugi24 <sutra.sugi@gmail.com>
2022-12-14 00:54:50 +0700

    method delete image

--- app/Http/Controllers/Dashboard/StudentController.php
+++ app/Http/Controllers/Dashboard/StudentController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Student;
+use Illuminate\Support\Facades\Storage;
 
 class StudentController extends Controller
 {
@@ -143,6 +144,7 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
+        
         $student = Student::find($id);
 
         return view('students.edit',[
@@ -159,15 +161,48 @@ class StudentController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'nama' => ['required', 'min:3', 'max:30'],
+            'nis' => ['required', 'min:5', 'numeric'],
+            'nisn' => ['required', 'min:5', 'numeric'],
+            'kelas' => ['required', 'min:3',],
+            'tahun' => ['required', 'min:4', 'numeric'],
+            'ijazah' => 'mimes:jpg,png,jpeg|file|max:3072',
+            'skhun' => 'mimes:jpg,png,jpeg|file|max:3072',
+            'status' => 'required'
+
+        ],[
+            'nama.required' => 'Kolom nama harus diisi.',
+            'nis.required' => 'Kolom Nomor Induk Siswa harus diisi.',
+            'nisn.required' => 'Kolom Nomor Induk Siswa harus diisi.',
+            'kelas.required' => 'Kolom Kelas harus diisi.',
+            'tahun.required' => 'Kolom Tahun harus diisi.',
+        ]);
+
         $student = Student::find($id);
+
+        if($request->file('ijazah')) {
+            $ijazah = $request->file('ijazah')->storeAs('ijazah-skhun', $request->nama.'-ijazah'. '.'.$request->file('ijazah')->extension());
+        } else{
+            $ijazah = $student->ijazah;
+        }
+
+
+        if($request->file('skhun')) {
+            $skhun = $request->file('skhun')->storeAs('ijazah-skhun', $request->nama.'-skhun'. '.'.$request->file('skhun')->extension());
+        } else{
+            $skhun = $student->skhun;
+        }
+
+        
 
         $student->nama = $request->nama;
         $student->nis = $request->nis;
         $student->nisn = $request->nisn;
         $student->kelas = $request->kelas;
         $student->tahun = $request->tahun;
-        $student->ijazah = $request->ijazah;
-        $student->skhun = $request->skhun;
+        $student->ijazah = $ijazah;
+        $student->skhun = $skhun;
         $student->status = $request->status;
 
         $student->save();
@@ -186,6 +221,16 @@ class StudentController extends Controller
     public function destroy($id)
     {
         $student = Student::find($id);
+        $pathIjazah = $student->ijazah;
+        $pathSkhun = $student->skhun;
+
+        if($pathIjazah != null || $pathIjazah != ''){
+            Storage::delete($pathIjazah);
+        }
+
+        if($pathSkhun != null || $pathSkhun != ''){
+            Storage::delete($pathSkhun);
+        }
 
         $student->delete();
 
