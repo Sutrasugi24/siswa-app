530e38fea7b4a14c5c173f40b294ce1a8b11423f
Sutrasugi24 <sutra.sugi@gmail.com>
2024-06-30 15:14:29 +0700

    update button in ijazah and shkun to download file

--- resources/views/students/ijazah.blade.php
+++ resources/views/students/ijazah.blade.php
@@ -1,22 +1,27 @@
 @extends('templates/default')
 
-
 @section('content')
 @if($student->ijazah)
     @php
-        $img = explode (",", $student->ijazah)
+        $img = explode(",", $student->ijazah);
     @endphp
     <div class="intro-y box overflow-hidden mt-5">
-    <h3 class="text-4xl font-normal leading-normal p-5 mt-5 mb-2 text-primary text-center"> Ijazah {{ $student->nama}}</h3>
-    <div class="p-8 flex items-center justify-center">
-    @foreach ($img as $key => $item)
-        @if(strpos($item, '.pdf'))
-            <embed class="w-full h-screen mr-3 ml-3" src="{{ asset('storage/ijazah/'. $item) }}" />
-        @else
-            <img class="w-full h-full mr-3 ml-3" src="{{ asset('storage/ijazah/'. $item) }}" alt="Tidak ada Gambar" data-action="zoom">
-        @endif
-    @endforeach
-    </div>
+        <h3 class="text-4xl font-normal leading-normal p-5 mt-5 mb-2 text-primary text-center"> Ijazah {{ $student->nama }}</h3>
+        <div class="p-8 flex items-center justify-center">
+            @foreach ($img as $key => $item)
+                @if(strpos($item, '.pdf'))
+                    <div class="relative w-full h-screen mr-3 ml-3">
+                        <embed class="w-full h-full" src="{{ asset('storage/ijazah/'. $item) }}" />
+                        <a href="{{ asset('storage/ijazah/'. $item) }}" download class="absolute top-0 right-0 p-2 bg-blue-500 text-white text-xs font-bold rounded-lg">Download</a>
+                    </div>
+                @else
+                    <div class="relative w-full h-full mr-3 ml-3">
+                        <img class="w-full h-full" src="{{ asset('storage/ijazah/'. $item) }}" alt="Tidak ada Gambar" data-action="zoom">
+                        <a href="{{ asset('storage/ijazah/'. $item) }}" download class="absolute top-0 right-0 p-2 bg-blue-500 text-white text-xs font-bold rounded-lg">Download</a>
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </div>
 @else
     <img src="#" alt="Tidak ada gambar">
--- resources/views/students/skhun.blade.php
+++ resources/views/students/skhun.blade.php
@@ -1,22 +1,26 @@
 @extends('templates/default')
-
-
 @section('content')
 @if($student->skhun)
     @php
-        $img = explode (",", $student->skhun)
+        $img = explode(",", $student->skhun);
     @endphp
     <div class="intro-y box overflow-hidden mt-5">
-    <h3 class="text-4xl font-normal leading-normal p-5 mt-5 mb-2 text-primary text-center"> SKHUN {{ $student->nama}}</h3>
-    <div class="p-8 flex items-center justify-center">
-    @foreach ($img as $key => $item)
-        @if(strpos($item, '.pdf'))
-            <embed class="w-full h-screen mr-3 ml-3" src="{{ asset('storage/skhun/'. $item) }}" />
-        @else
-            <img class="w-full h-full mr-3 ml-3" src="{{ asset('storage/skhun/'. $item) }}" alt="Tidak ada Gambar" data-action="zoom">
-        @endif
-    @endforeach
-    </div>
+        <h3 class="text-4xl font-normal leading-normal p-5 mt-5 mb-2 text-primary text-center"> SKHUN {{ $student->nama }}</h3>
+        <div class="p-8 flex items-center justify-center">
+            @foreach ($img as $key => $item)
+                @php
+                    $fileUrl = asset('storage/skhun/'. $item);
+                @endphp
+                @if (strpos($item, '.pdf'))
+                    <embed class="w-full h-screen mr-3 ml-3" src="{{ $fileUrl }}" />
+                @else
+                    <div class="relative">
+                        <img class="w-full h-full mr-3 ml-3" src="{{ $fileUrl }}" alt="Tidak ada Gambar" data-action="zoom">
+                        <a href="{{ $fileUrl }}" download="{{ $item }}" class="absolute top-0 right-0 p-2 bg-blue-500 text-white text-xs font-bold rounded-lg">Download</a>
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </div>
 @else
     <img src="#" alt="Tidak ada gambar">
